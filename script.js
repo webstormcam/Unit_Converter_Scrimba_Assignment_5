3812c0e09ef2afe0f9378052e167a85fbb55d9a5
Cameron Chandler <webstormcam@gmail.com>
2023-01-06 21:18:27 -0500

    Be working on the JS

---
+++ script.js
@@ -0,0 +1,45 @@
+const convert = document.getElementById('convert')
+let inputted = document.getElementById('enteredValue')
+let inputValue = document.getElementsByClassName('input-value')
+
+const feet = document.getElementById('feet')
+const meter = document.getElementById('meters')
+const liters = document.getElementById('liters')
+const gallons = document.getElementById('gallons')
+
+convert.addEventListener('click',function(){
+    if(inputted.value===""){
+        return
+    }
+    for(let i=0;i<inputValue.length;i++){
+        inputValue[i].innerHTML=inputted.value;
+    }
+
+    MetersToFeet(inputted)
+    FeetToMeters(inputted)
+    litersToGallons(inputted)
+    gallonstoLiters(inputted)
+    
+})
+
+
+function MetersToFeet(value){
+let newNum= value.value*3.281
+newNum= newNum.toFixed(3)
+feet.innerHTML=newNum
+
+}
+
+function FeetToMeters(value){
+    const newNum = value.value/3.281
+    meter.innerHTML=newNum.toFixed(3)
+}
+
+function litersToGallons(value){
+    const newNum = value.value*0.264
+    gallons.innerHTML=newNum.toFixed(3)
+}
+function gallonstoLiters(value){
+    const newNum = value.value/0.264
+    liters.innerHTML=newNum.toFixed(3)
+}
